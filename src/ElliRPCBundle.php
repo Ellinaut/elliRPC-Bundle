a6ff7645b15fe203323c2628f635d23ca2e62dc1
pmarien <philipp.marien@gmail.com>
2021-04-08 21:37:45 +0200

    service configuration

--- src/ElliRPCBundle.php
+++ src/ElliRPCBundle.php
@@ -2,6 +2,11 @@
 
 namespace Ellinaut\ElliRPCBundle;
 
+use Ellinaut\ElliRPCBundle\DependencyInjection\Compiler\ProcedureProcessorPass;
+use Ellinaut\ElliRPCBundle\DependencyInjection\Compiler\RequestParserPass;
+use Ellinaut\ElliRPCBundle\DependencyInjection\Compiler\RequestProcessorPass;
+use Ellinaut\ElliRPCBundle\DependencyInjection\Compiler\ResponseFactoryPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -9,5 +14,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ElliRPCBundle extends Bundle
 {
-
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new ProcedureProcessorPass());
+        $container->addCompilerPass(new RequestParserPass());
+        $container->addCompilerPass(new RequestProcessorPass());
+        $container->addCompilerPass(new ResponseFactoryPass());
+    }
 }
